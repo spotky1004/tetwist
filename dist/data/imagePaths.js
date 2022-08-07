49adb6d69d5dc9f366317c7efc7b73c916c65e41
Spotky1004 <spotky1004@gmail.com>
2022-08-07 13:32:54 +0900

    Clean

--- dist/data/imagePaths.js
+++
@@ -1,7 +0,0 @@
-import useRelativePath from "../util/path/useRelativePath.js";
-const imagePaths = {
-    bgNormal: useRelativePath("../../assets/images/bg_normal.png"),
-    bgColor: useRelativePath("../../assets/images/bg_color.png"),
-    tetwistTileset: useRelativePath("../../assets/images/tetwist_tileset.png")
-};
-export default imagePaths;
--- dist/util/create2DArray.js
+++
@@ -1,7 +0,0 @@
-export default function create2DArray(rows, cols, fill) {
-    const array = new Array(cols)
-        .fill(undefined)
-        .map(_ => new Array(rows)
-        .fill(typeof fill !== "undefined" ? fill : undefined));
-    return array;
-}
