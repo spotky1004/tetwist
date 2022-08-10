c8138d420db6dbb61d6d1d63496458c90996704c
Spotky1004 <spotky1004@gmail.com>
2022-08-11 05:39:48 +0900

    Create Scheduler class

---
+++ dist/class/util/Scheduler.js
@@ -0,0 +1,38 @@
+export default class Scheduler {
+    constructor() {
+        this._time = 0;
+        this.tasks = [];
+    }
+    tick(dt) {
+        this._time += dt;
+        const time = this._time;
+        for (const task of this.tasks) {
+            const [callback, [startedAt, delay, interval, totalCallCount]] = task;
+            const nextCallTime = startedAt + delay + interval * totalCallCount;
+            if (time < nextCallTime)
+                continue;
+            const callCount = interval > 0 ? Math.floor((time - nextCallTime) / interval) + 1 : 1;
+            for (let i = 0; i < callCount; i++) {
+                callback(totalCallCount + i);
+            }
+            task[1][3] += callCount;
+        }
+    }
+    addSchedule(callback, delay, interval) {
+        const task = [
+            callback,
+            [this._time, delay, interval, 0]
+        ];
+        this.tasks.push(task);
+        return this;
+    }
+    get time() {
+        return this._time;
+    }
+    restart() {
+        this._time = 0;
+    }
+    clearSchedule() {
+        this.tasks = [];
+    }
+}
---
+++ src/class/util/Scheduler.ts
@@ -0,0 +1,48 @@
+type SchedulerTaskTimes = [startedAt: number, delay: number, interval: number, totalCallCount: number];
+type SchedulerFunction = (repeatCount: number) => boolean;
+type SchedulerTask = [callback: SchedulerFunction, times: SchedulerTaskTimes];
+
+export default class Scheduler {
+  private _time: number = 0;
+  private tasks: SchedulerTask[] = [];
+  
+  constructor() {
+  }
+  
+  tick(dt: number) {
+    this._time += dt;
+    const time = this._time;
+    for (const task of this.tasks) {
+      const [callback, [startedAt, delay, interval, totalCallCount]] = task;
+      const nextCallTime = startedAt + delay + interval * totalCallCount;
+      if (time < nextCallTime) continue;
+      const callCount = interval > 0 ? Math.floor((time - nextCallTime) / interval) + 1 : 1;
+      for (let i = 0; i < callCount; i++) {
+        callback(totalCallCount + i);
+      }
+      task[1][3] += callCount;
+    }
+  }
+
+  addSchedule(callback: Function, delay: number, interval: number) {
+    const task: SchedulerTask = [
+      callback as SchedulerFunction,
+      [this._time, delay, interval, 0]
+    ];
+    this.tasks.push(task);
+
+    return this;
+  }
+
+  get time() {
+    return this._time;
+  }
+
+  restart() {
+    this._time = 0;
+  }
+
+  clearSchedule() {
+    this.tasks = [];
+  }
+}
