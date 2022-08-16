bacf3889d5bf5a736bfb0876473c5737420e73ed
Spotky1004 <spotky1004@gmail.com>
2022-08-16 10:19:10 +0900

    Remove field test from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import RepeatingBackground from "./class/util/RepeatingBackground.js";
 import imagePaths from "./data/imagePaths.js";
-import tilesEnum from "./data/tilesEnum.js";
 import Game from "./class/game/Game.js";
 
 const game = new Game({
@@ -11,26 +10,6 @@ const game = new Game({
     canvas: document.getElementById("game__tetwist-field") as HTMLCanvasElement,
   }
 });
-// field test
-const field = game.field;
-for (let x = 0; x < 10; x++) {
-  for (let y = 0; y < 20; y++) {
-    if (Math.random() > 0.5) continue;
-    const timestamp = Math.floor(Math.random()*2);
-    const tileId = tilesEnum.normalBlock;
-    const isGrey = 0;
-    field.setCell(x, y, {
-      isSolid: true,
-      tileId,
-      timestemp: timestamp,
-      hslAdjust: {
-        hue: Math.random()*360,
-        lightness: isGrey ? 0 : 160,
-        saturation: isGrey ? -50 : 0
-      }
-    });
-  }
-}
 
 // create background
 const background = new RepeatingBackground({
