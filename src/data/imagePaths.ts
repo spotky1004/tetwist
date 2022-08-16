7447bad05c7e1d8ca3e61090f4c386916f7b6128
Spotky1004 <spotky1004@gmail.com>
2022-08-16 18:07:38 +0900

    Change block skin

--- src/data/imagePaths.ts
+++ src/data/imagePaths.ts
@@ -3,6 +3,6 @@ import useRelativePath from "../util/etc/path/useRelativePath.js";
 const imagePaths = {
   bgNormal: useRelativePath("../../assets/images/bg_normal.png"),
   bgColor: useRelativePath("../../assets/images/bg_color.png"),
-  blockTileset: useRelativePath("../../assets/images/block_tileset_grid_filled.png")
+  blockTileset: useRelativePath("../../assets/images/block_tileset_grid.png")
 }
 export default imagePaths;
