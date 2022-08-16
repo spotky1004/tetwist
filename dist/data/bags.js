9ba6608042b3df3d40de8feeb71f432c278d0d4b
Spotky1004 <spotky1004@gmail.com>
2022-08-16 17:54:54 +0900

    Create bags.bag7

---
+++ dist/data/bags.js
@@ -0,0 +1,14 @@
+import Bag from "../class/game/piece/Bag.js";
+import tetrominos from "./pieces/tetrominos.js";
+const bag7 = new Bag([
+    tetrominos.iPiece,
+    tetrominos.jPiece,
+    tetrominos.lPiece,
+    tetrominos.oPiece,
+    tetrominos.sPiece,
+    tetrominos.tPiece,
+    tetrominos.zPiece
+]);
+export default {
+    bag7
+};
